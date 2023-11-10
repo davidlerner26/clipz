8072f88922b2a4ac6d77e1b06577f48b5202e43b
davidlerner26 <david.lerner@hotmail.com>
2023-11-10 18:04:46 -0300

    Handle validation for password input

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -12,8 +12,8 @@ export class RegisterComponent {
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
     email: new FormControl('', [Validators.required, Validators.email]),
     age: new FormControl('', [Validators.required, Validators.min(18), Validators.max(120)]),
-    password: new FormControl(''),
-    confirm_password: new FormControl(''),
+    password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)]),
+    confirm_password: new FormControl('', [Validators.required]),
     phoneNumber: new FormControl('')
   });
 
