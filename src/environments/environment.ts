25314aa26edde5597658efabc3bf1b0483252798
davidlerner26 <david.lerner@hotmail.com>
2023-11-23 12:27:52 -0300

    Customize env for angular fire

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyA5gR6RTFwvNJJTTR0Irqu3hzAE4Bz77VQ",
+        authDomain: "clipz-fd6fb.firebaseapp.com",
+        projectId: "clipz-fd6fb",
+        storageBucket: "clipz-fd6fb.appspot.com",
+        appId: "1:424332989480:web:ef24db87cb6abc9986111d"
+    }
+}
