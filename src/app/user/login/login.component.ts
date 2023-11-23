f2043c9b40d1291e5b60e610b1a691fd181ce045
davidlerner26 <david.lerner@hotmail.com>
2023-11-23 12:58:17 -0300

    Login user with firebase

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
@@ -11,9 +13,29 @@ export class LoginComponent {
     email: '',
     password: ''
   }
+  showAlert = false;
+  alertMsg = 'Please wait, we are logging you in.';
+  alertColor = 'blue';
+  inSubmission = false;
 
-  login() {
+  constructor(private auth: AngularFireAuth) { }
 
+  async login() {
+    this.showAlert = true;
+    this.alertMsg = 'Please wait, we are logging you in.';
+    this.alertColor = 'blue';
+    this.inSubmission = true;
+
+    try {
+      await this.auth.signInWithEmailAndPassword(this.credentials.email, this.credentials.password);
+      this.alertMsg = 'Sucess! Your account has been created!';
+      this.alertColor = 'green';
+    } catch (error) {
+      console.error(error);
+      this.inSubmission = false;
+      this.alertMsg = 'An unexpected error ocurred. Please try again later.';
+      this.alertColor = 'red';
+    }
   }
 
 }
