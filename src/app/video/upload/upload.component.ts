949ead9b8b68e8dedcda2bb400f2c307b428460e
davidlerner26 <david.lerner@hotmail.com>
2023-11-24 11:16:23 -0300

    Drag and drop mp4 files

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class UploadComponent {
 
+  isDragover = false;
+  file: File | null = null;
+  nextStep = false;
+
+  constructor() { }
+
+  storeFile($event: Event) {
+    this.isDragover = false;
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
+
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return;
+    }
+    console.log(this.file);
+
+    this.nextStep = true;
+  }
+
 }
