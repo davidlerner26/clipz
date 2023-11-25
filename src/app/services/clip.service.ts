252150b04543e9ea99df0a788f535c7ae0505e7c
davidlerner26 <david.lerner@hotmail.com>
2023-11-25 19:03:58 -0300

    Sort clips based on timestamp

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, DocumentReference, QuerySnapshot } from '@angular/fire/compat/firestore';
 import { IClip } from '../models/clip.model';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { map, of, switchMap } from 'rxjs';
+import { BehaviorSubject, combineLatest, map, of, switchMap } from 'rxjs';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 @Injectable({
@@ -24,18 +24,19 @@ export class ClipService {
     return this.clipsCollection.add(data);
   }
 
-  getUserClips() {
-    return this.auth.user.pipe(
-      switchMap(user => {
+  getUserClips(sort$: BehaviorSubject<string>) {
+    return combineLatest([this.auth.user, sort$]).pipe(
+      switchMap(values => {
+        const [user, sort] = values;
         if (!user) {
           return of([]);
         }
 
         const query = this.clipsCollection.ref.where(
           'uid', '==', user.uid
-        )
+        ).orderBy('timestamp', sort === '1' ? 'desc' : 'asc')
 
-        return query.get()
+        return query.get();
       }),
       map(snapshot => (snapshot as QuerySnapshot<IClip>).docs)
     )
