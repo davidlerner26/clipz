50c27a22ac118612e4e3bca787c5d85b8ffbb4ec
davidlerner26 <david.lerner@hotmail.com>
2023-12-08 19:40:43 -0300

    Change video tag css with video.js

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-
+import videojs from "video.js";
+import Player from 'video.js/dist/types/player';
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
-  styleUrls: ['./clip.component.scss']
+  styleUrls: ['./clip.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ClipComponent implements OnInit {
 
+  @ViewChild('videoPlayer', { static: true }) target?: ElementRef;
+  player?: Player;
   id = '';
 
   constructor(public route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.player = videojs.getPlayer(this.target?.nativeElement);
     this.id = this.route.snapshot.params['id'];
     this.route.params.subscribe((params: Params) => {
       this.id = params['id'];
     })
   }
+
+
 }
