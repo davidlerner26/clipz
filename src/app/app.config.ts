63c459b76c758a90e930929179396d580cfda3fb
David Lerner <david.lerner@hotmail.com>
2026-03-20 10:24:16 -0300

    Add firebase configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,7 +11,16 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideFirebaseApp(() => initializeApp({})),
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: 'AIzaSyA5gR6RTFwvNJJTTR0Irqu3hzAE4Bz77VQ',
+        authDomain: 'clipz-fd6fb.firebaseapp.com',
+        projectId: 'clipz-fd6fb',
+        storageBucket: 'clipz-fd6fb.appspot.com',
+        messagingSenderId: '424332989480',
+        appId: '1:424332989480:web:ef24db87cb6abc9986111d',
+      }),
+    ),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
